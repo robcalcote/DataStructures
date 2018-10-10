4532d65cb601b931b271e10db514792abd25f3f3
Rob Calcote <robcalcote@gmail.com>
2018-10-09 22:31:51 -0600

    added Controller with a couple of methods

---
+++ DataStructures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // If clicked, method will return the user to www.espn.com
+        public ActionResult Exit()
+        {
+            return Redirect("http://www.espn.com");
+        }
+    }
+}
