a7799ed5b3b48942b506b3551137c511963c69f0
Rob Calcote <robcalcote@gmail.com>
2018-10-09 22:45:21 -0600

    added Stack controller and view

---
+++ DataStructures/Controllers/StackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructures.Controllers
+{
+    public class StackController : Controller
+    {
+        // GET: Stack
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
